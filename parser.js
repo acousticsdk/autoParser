32d1ddb13ee0122fdf67210f972157201585168c
Максим <acoustic.sdk@gmail.com>
2025-01-25 03:36:36 +0200

    4

--- parser.js
+++ parser.js
@@ -41,7 +41,7 @@ const MIN_SIZE = 11;
 const MAX_SIZE = 60;
 
 // Update interval (in milliseconds)
-const UPDATE_INTERVAL = 8 * 60 * 1000; // 8 minutes between full update
+const UPDATE_INTERVAL = 10 * 60 * 1000; // 8 minutes between full update
 
 // Fresh listings threshold (in minutes)
 const FRESH_LISTING_THRESHOLD = 59;
