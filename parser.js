81ffcc9f46cf890bb537587bccec7ec2c32a263d
Максим <acoustic.sdk@gmail.com>
2025-01-30 16:50:29 +0200

    1

--- parser.js
+++ parser.js
@@ -45,7 +45,7 @@ const MAX_SIZE = 60;
 const UPDATE_INTERVAL = 10 * 60 * 1000; // 10 minutes between full update
 
 // Fresh listings threshold (in minutes)
-const FRESH_LISTING_THRESHOLD = 50;
+const FRESH_LISTING_THRESHOLD = 59;
 
 // SMS sending time window
 const SMS_START_HOUR = 10;
